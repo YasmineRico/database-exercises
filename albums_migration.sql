21492d0806be05919b1b0e100f4f19005792f6ac
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-18 14:04:02 -0500

    complete database exercise for albums_migration.sql

---
+++ albums_migration.sql
@@ -0,0 +1,16 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT(6),
+    sales FLOAT NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DESCRIBE albums;
+
