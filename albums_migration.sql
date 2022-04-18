9d669de34d4adbe426509921766b79fcd664a53b
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-18 17:07:23 -0500

    add insertion of album names

--- albums_migration.sql
+++ albums_migration.sql
@@ -12,6 +12,18 @@ CREATE TABLE albums(
     PRIMARY KEY (id)
 );
 
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Fall Out Boy', 'From Under The Cork Tree', 2005, 233, 'Alternative Rock');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Flyleaf', 'Flyleaf', 2006, 22323, 'Alternative Rock');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES
+('Paramore', 'Riot!', 2007, 282828, 'Alternative');
+
+INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Bad Bunny', 'YHLQMDLG', 2020, 83838338, 'Reggaeton');
+
+
+
 DESCRIBE albums;
 SHOW CREATE TABLE albums;
 
