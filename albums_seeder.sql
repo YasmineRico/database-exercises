6fcd78e612c84afe7cc8ed581b187e49b04a46b6
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-18 17:08:44 -0500

    begin adding records from wikipedia page

---
+++ albums_seeder.sql
@@ -0,0 +1,52 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 49.2, 'Pop, rock, R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits 1971-1975', 1976, 41.2, 'Country Rock, Soft Rock, Folk Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, Soul, Pop, Soundtrack');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Eagles', 'Hotel California', 1976, 31.5, 'Soft Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('AC/DC', 'Back in Black', 1980, 29.5, 'Hard Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'Progressive Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Bee Gees', 'Saturday Night Fever', 1977, 21.6, 'Disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Various Artist', 'Grease: The Original Soundtrack from the Motion Picture', 1978, 14.4, 'Rock and Roll');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Led Zepplin', 'Led Zeppelin IV', 1971, 29, 'Hard rock, Heavy Metal, Folk Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Michael Jackson', 'Bad', 1987, 21.1, 'Pop, Rhythm and Blues, Funk and Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Alanis Morissette', 'Jagged Little Pill', 1995, 24.3, 'Alternative Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Michael Jackson', 'Dangerous', 1991, 17, 'New Jack Swing, R&B, Pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('Celine Dion', 'Falling into You', 1996, 21.1, 'Pop, Soft Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18.2, 'Rock');
+
+INSERT INTO albums (artist, name, release_data, sales, genre)
+VALUES ('Elton John', 'Goodbye Yellow Brick Road', 1973, 8.5, 'rock, pop rock, glam rock');
