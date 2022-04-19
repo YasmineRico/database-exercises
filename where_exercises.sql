086e0f6d858b684e72127a9c4e4d4f133e9f4275
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 15:05:14 -0500

    complete part two first section 'using OR instead of IN'

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,7 +4,9 @@ USE employees;
 #Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 SELECT *
 FROM employees
-where first_name IN ('Irena', 'Vidya', 'Maya');
+where (first_name ='Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
 
 
 #Find all employees whose last name starts with 'E' — 7,330 rows.
@@ -16,3 +18,14 @@ WHERE last_name LIKE 'E%';
 SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
+
+#Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+
+
+#Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+
+#Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+
+#Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+
+#Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
