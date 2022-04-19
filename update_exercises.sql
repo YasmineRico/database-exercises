1924c26e52b84ade6e5903fb13b3235ac8436d7f
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 09:16:18 -0500

    begin exercise

---
+++ update_exercises.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums;
+
+
