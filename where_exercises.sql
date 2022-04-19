130ec0817cae566a0089b0fc98738f7d5ef178a0
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 15:19:39 -0500

    complete all exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,7 +6,8 @@ SELECT *
 FROM employees
 where (first_name ='Irena'
 OR first_name = 'Vidya'
-OR first_name = 'Maya');
+OR first_name = 'Maya')
+AND gender ='M';
 
 
 #Find all employees whose last name starts with 'E' — 7,330 rows.
@@ -19,13 +20,25 @@ SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
 
-#Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+#Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows. -- Changed line 5
 
 
-#Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
+#Add a condition to the previous query to find everybody with those names who is also male — 441 rows. -- changed line 5
 
 #Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%E';
 
 #Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT last_name
+From employees
+Where last_name LIKE 'E%'
+  AND last_name Like '%E';
 
 #Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+          AND last_name NOT LIKE '%qu%';
