90a65d7361d42f3ed3853eb220c0e99106d935c0
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 15:00:51 -0500

    complete third section

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,6 +10,9 @@ where first_name IN ('Irena', 'Vidya', 'Maya');
 #Find all employees whose last name starts with 'E' — 7,330 rows.
 SELECT *
 FROM employees
-WHERE last_name LIKE 'E%'
+WHERE last_name LIKE 'E%';
 
 #Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
