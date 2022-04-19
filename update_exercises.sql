dca29af85fd23d86bc66894582afa2e0e10a231c
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 09:51:02 -0500

    complete exercise

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,5 +1,8 @@
 USE codeup_test_db;
 
+# ***** Add SELECT statements after each UPDATE so you can see the results of your handiwork.
+
+# All albums in your table.
 SELECT id AS "ID",
        artist AS "Artist",
        name AS "Album Name",
@@ -7,5 +10,67 @@ SELECT id AS "ID",
        sales AS "Sales",
        genre AS "Genre"
 FROM albums;
+
+#All albums released before 1980
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE release_date < 1980;
+
+#All albums by Michael Jackson
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE artist = 'Michael Jackson';
+
+#Make all the albums 10 times more popular (sales * 10)
+UPDATE albums
+SET sales = sales * 10;
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums;
+
+#Move all the albums before 1980 back to the 1800s.
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date <1980;
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE release_date < 1980;
+
+#Change 'Michael Jackson' to 'Peter Jackson'
+UPDATE albums
+SET artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE artist = 'Peter Jackson';
+
+
+
+
+
 
 
