0d89c23685966631a539f1c90175e7d1c76a5f95
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-19 09:51:26 -0500

    complete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,42 @@
+USE codeup_test_db;
+
+#Albums released after 1991
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE release_date > 1991;
+DELETE
+FROM albums
+WHERE release_date > 1991;
+
+#Albums with the genre 'disco'
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE genre = 'Disco';
+
+DELETE
+FROM albums
+WHERE genre = 'Disco';
+
+#Albums by 'Whitney Houston' (...or maybe an artist of your choice)
+SELECT id AS "ID",
+       artist AS "Artist",
+       name AS "Album Name",
+       release_date AS "Year Released",
+       sales AS "Sales",
+       genre AS "Genre"
+FROM albums
+WHERE artist = 'Whitney Houston';
+
+DELETE
+FROM albums
+WHERE artist = 'Whitney Houston';
