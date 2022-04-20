4f969297b9601fb0875f4b5555d8180d07efc5fc
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-20 10:49:29 -0500

    revise first solution

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,10 +1,23 @@
 USE employees;
 
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT emp_no FROM salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT emp_no FROM salaries
+ORDER BY salary DESC
+LIMIT 5 OFFSET 45;
+
 #Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
 SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name'
 FROM employees
-WHERE last_name LIKE 'E%'
-  AND last_name LIKE '%E';
+WHERE last_name LIKE 'e%e';
+
 #Find all employees born on Christmas — 842 rows.
 
 #Find all employees hired in the 90s and born on Christmas — 362 rows.
