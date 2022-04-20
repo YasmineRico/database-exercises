3f3bcb9bcfaa3538a0e82bee689284649b8097ce
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-20 14:04:52 -0500

    complete exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -19,9 +19,33 @@ FROM employees
 WHERE last_name LIKE 'e%e';
 
 #Find all employees born on Christmas — 842 rows.
+SELECT COUNT(*) FROM employees WHERE MONTH(birth_date)= 12 AND DAY(birth_date)= 25;
+
+#OR
+SELECT COUNT(*) FROM employees WHERE birth_date LIKE '%-12-25';
 
 #Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT COUNT(*) FROM employees WHERE MONTH(birth_date) =12
+AND DAY(birth_date)= 25
+AND hire_date  LIKE '199%';
 
 #Change the query for employees hired in the 90s and born on Christmas such that the first result is the oldest employee who was hired last. It should be Khun Bernini.
+SELECT  first_name, last_name
+FROM employees
+WHERE MONTH(birth_date)=12
+AND DAY(birth_date)=25
+AND hire_date Like '199%'
+ORDER BY birth_date, hire_date DESC;
+
+#If you only wanted to return one employee you can do a limit clause
+#LIMIT 1
 
 #For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You might also need to use now() or curdate()).
+
+
+SELECT first_name, last_name, CONCAT(datediff(now(), hire_date), ' days')
+FROM employees
+WHERE month(birth_date) = 12
+  AND day(birth_date) = 25
+  AND year(hire_date) BETWEEN 1990 and 1999
+ORDER BY datediff(now(), hire_date) DESC;
