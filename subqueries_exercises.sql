98b0f9a131c739098941af2f6af082a6e7de8361
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-22 10:17:48 -0500

    add solution to second question in subqueries_exercises.sql

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -8,7 +8,13 @@ WHERE hire_date = (SELECT hire_date from employees where emp_no = 101010);
 
 #Find all the titles held by all employees with the first name Aamod. 314 total titles, 6 unique titles
 
-
+SELECT DISTINCT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+);
 
 
 #Find all the current department managers that are female.
